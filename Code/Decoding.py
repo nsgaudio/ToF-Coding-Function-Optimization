52be4fdecdacdc846ca64492e4487bb566cc1438
Nicholas Gaudio <nsgaudio@stanford.edu>
2019-05-11 17:05:32 -0700

    Updated Decoding.

--- Code/Decoding.py
+++ Code/Decoding.py
@@ -25,16 +25,15 @@ def DecodeXCorr(BMeasurements, NormCorrFs):
 	B, K = BMeasurements_reshaped.shape
 	N = NormCorrFs.shape[0]
 	## Normalize Brightness Measurements functions
-	# NormBMeasurements = (BMeasurements.transpose() - np.mean(BMeasurements, axis=1)) / np.std(BMeasurements, axis=1)
 	NormBMeasurements_reshaped = (BMeasurements_reshaped.t() - torch.mean(BMeasurements_reshaped, dim=1)) / torch.std(BMeasurements_reshaped, dim=1)
 	## Calculate the cross correlation for every measurement and the maximum one will be the depth
-	# decodedDepths = torch.zeros((NormBMeasurements.shape[1],), dtype=torch.float32)
 	decodedDepths_reshaped = torch.zeros((B,), dtype=torch.float32)
-	enum = torch.linspace(0, N - 1, steps=N)
+	enumeration = torch.linspace(0, N - 1, steps=N)
+	beta = 10
 	for i in range(B):
-		see = torch.mv(NormCorrFs, NormBMeasurements_reshaped[:,i])
+		Corr_B = torch.mv(NormCorrFs, NormBMeasurements_reshaped[:,i])
 		SM = torch.nn.Softmax()
-		temp = SM(see)
-		decodedDepths_reshaped[i] = torch.dot(temp, enum)
+		Confidence = SM(Corr_B * beta)
+		decodedDepths_reshaped[i] = torch.dot(Confidence, enumeration)
 	decodedDepths = torch.reshape(decodedDepths_reshaped, (C, H, W))
 	return decodedDepths

568b9883985de923d40075ff7e13fcd31bc493d6
messnerj <messnerj@stanford.edu>
2019-05-14 15:37:28 -0700

    added file save

--- ToFSim/test.py
+++ ToFSim/test.py
@@ -1,12 +1,15 @@
-
+import numpy as np
+import scipy.io as sio
 import CodingFunctions
 import UtilsPlot
+import Utils
 
-N = 1000
+N = 10000
 K = 3
+(ModFs,DemodFs) = CodingFunctions.GetCodingFromFile('coding_functions.npz')
 # (ModFs,DemodFs) = CodingFunctions.GetCosCos(N = N, K = K)
 #(ModFs,DemodFs) = CodingFunctions.GetBinaryFunc(N = N, K = K)
-(ModFs,DemodFs) = CodingFunctions.GetSquare(N = N, K = K)
+#(ModFs,DemodFs) = CodingFunctions.GetSquare(N = N, K = K)
 #(ModFs,DemodFs) = CodingFunctions.GetHamK3(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK4(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK5(N = N)
@@ -14,6 +17,5 @@ K = 3
 
 
 UtilsPlot.PlotCodingScheme(ModFs,DemodFs)
-
-
-
+CorrFs = Utils.GetCorrelationFunctions(ModFs, DemodFs)
+sio.savemat('coding_functions.mat', {'ModFs':ModFs, 'DemodFs':DemodFs, 'CorrFs':CorrFs})

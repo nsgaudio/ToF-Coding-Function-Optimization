fixed normalization bug

Code/Decoding.py
@@ -4,6 +4,8 @@ Decoding functions for time of flight coding schemes.
 #### Python imports
 
 #### Library imports
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -28,16 +30,19 @@ def DecodeXCorr(BMeasurements, NormCorrFs):
 	BMeasurements_reshaped = torch.reshape(BMeasurements, (-1, K))
 	B, K = BMeasurements_reshaped.shape
 	N = NormCorrFs.shape[0]
+
 	## Normalize Brightness Measurements functions
 	NormBMeasurements_reshaped = (BMeasurements_reshaped.t() - torch.mean(BMeasurements_reshaped, dim=1)) / torch.std(BMeasurements_reshaped, dim=1)
+
 	## Calculate the cross correlation for every measurement and the maximum one will be the depth
-	decodedDepths_reshaped = torch.zeros((B,), dtype=torch.float32)
+	decodedDepths_reshaped = torch.zeros((B,), dtype=torch.float)
 	enumeration = torch.linspace(0, N - 1, steps=N)
-	beta = 10
+	beta = 20
 	for i in range(B):
 		Corr_B = torch.mv(NormCorrFs, NormBMeasurements_reshaped[:,i])
 		SM = torch.nn.Softmax(dim=0)
 		Confidence = SM(Corr_B * beta)
 		decodedDepths_reshaped[i] = torch.dot(Confidence, enumeration)
+		#decodedDepths_reshaped[i] = torch.argmax(Corr_B,dim=0)
 	decodedDepths = torch.reshape(decodedDepths_reshaped, (C, H, W))
 	return decodedDepths

Code/script_3_working.py
@@ -32,11 +32,19 @@ class Pixelwise(torch.nn.Module):
         ambientExponent = 6
 
         #### Coding
+        #N = 10000
+        #K = 3
+        #self.ModFs = torch.cat((2*torch.ones((int(N/2), 3), device=device, dtype=dtype), torch.zeros((int(N/2),3), device=device, dtype=dtype)),0)
+        #temp = 1/np.power(10, 0) * torch.rand(N, K, device=device, dtype=dtype, requires_grad=True) # scaled random initialization
+        #temp = torch.zeros((N, K), device=device, dtype=dtype)
+        #self.DemodFs = temp.clone().detach().requires_grad_(True)
+
+        #### Coding (Initialize at Hamiltonian)
         N = 10000
         K = 3
-        self.ModFs = torch.cat((2*torch.ones((int(N/2), 3), device=device, dtype=dtype), torch.zeros((int(N/2),3), device=device, dtype=dtype)),0)
-        temp = 1/np.power(10, 0) * torch.rand(N, K, device=device, dtype=dtype, requires_grad=True) # scaled random initialization
-        #temp = torch.zeros((N, K), device=device, dtype=dtype)
+        (ModFs_np,DemodFs_np) = CodingFunctions.GetHamK3(N = N)
+        self.ModFs = torch.tensor(ModFs_np, device=device, dtype=dtype)
+        temp = torch.tensor(DemodFs_np, device=device, dtype=dtype)
         self.DemodFs = temp.clone().detach().requires_grad_(True)
 
         #### Global parameters
@@ -77,7 +85,9 @@ class Pixelwise(torch.nn.Module):
         DemodFs_clipped = torch.clamp(self.DemodFs, 0.0, 1.0)
         # Calculate correlation functions (NxK matrix) and normalize it (zero mean, unit variance)
         CorrFs = Utils.GetCorrelationFunctions(ModFs_scaled,DemodFs_clipped,dt=self.dt)
-        NormCorrFs = (CorrFs - torch.mean(CorrFs,0)) / torch.std(CorrFs,0)
+        NormCorrFs = (CorrFs.t() - torch.mean(CorrFs,1)) / torch.std(CorrFs,1)
+        NormCorrFs = NormCorrFs.t()
+        # Compute brightness values
         BVals = Utils.ComputeBrightnessVals(ModFs=ModFs_scaled, DemodFs=DemodFs_clipped, CorrFs=CorrFs, depths=gt_depths, \
                 pAmbient=self.pAveAmbientPerPixel, beta=self.meanBeta, T=self.T, tau=self.tau, dt=self.dt, gamma=self.gamma)
         
@@ -95,8 +105,8 @@ class Pixelwise(torch.nn.Module):
 
 # Create random Tensors to hold inputs and outputs
 N = 1
-H = 5
-W = 5
+H = 10
+W = 10
 gt_depths = 9000*torch.rand(N, H, W, device=device, dtype=dtype, requires_grad=True)
 print('gt_depths:',gt_depths)
 
@@ -107,11 +117,11 @@ model = Pixelwise()
 # in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
 criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = optim.Adam([model.DemodFs], lr = 5e-2)
+optimizer = optim.Adam([model.DemodFs], lr = 5e-3)
 
 
 with torch.autograd.detect_anomaly():
-    for t in range(2000):
+    for t in range(1000):
         # Forward pass: Compute predicted y by passing x to the model
         depths_pred = model(gt_depths)
 